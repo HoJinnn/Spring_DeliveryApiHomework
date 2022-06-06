07c21513ec1c1698b8bed3ee2d4c2995f52d6256
printf-jin <dlghwls7662@naver.com>
2022-06-06 15:34:52 +0900

    Menu엔티티 수정
    UniqueConstraint 제약 조건 추가

--- src/main/java/com/sparta/deliveryapihomework/model/Menu.java
+++ src/main/java/com/sparta/deliveryapihomework/model/Menu.java
@@ -5,12 +5,16 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
+@Table(uniqueConstraints =
+        @UniqueConstraint(
+        name = "MENU_UNIQUE",
+        columnNames = {"NAME", "RESTAURANT_ID"}))
 @NoArgsConstructor
 @Getter
 @Entity
 public class Menu extends Timestamped {
 
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
     private Long id;
 
@@ -20,7 +24,14 @@ public class Menu extends Timestamped {
     @Column(nullable = false)
     private int price;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "restaurant_id")
+    @ManyToOne
+    @JoinColumn(name = "RESTAURANT_ID")
     private Restaurant restaurant;
+
+
+    public Menu(String name, int price, Restaurant restaurant) {
+        this.name = name;
+        this.price = price;
+        this.restaurant = restaurant;
+    }
 }
