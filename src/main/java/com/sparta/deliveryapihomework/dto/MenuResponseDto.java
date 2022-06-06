8c83eebeeecbd4aeef3175596259d63699f3e608
printf-jin <dlghwls7662@naver.com>
2022-06-06 15:33:26 +0900

    MenuResponseDto수정

--- src/main/java/com/sparta/deliveryapihomework/dto/MenuResponseDto.java
+++ src/main/java/com/sparta/deliveryapihomework/dto/MenuResponseDto.java
@@ -1,2 +1,17 @@
-package com.sparta.deliveryapihomework.dto;public class MenuResponseDto {
+package com.sparta.deliveryapihomework.dto;
+
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+public class MenuResponseDto {
+    private Long id;
+    private String name;
+    private int price;
+
+    public MenuResponseDto(Long id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 }
