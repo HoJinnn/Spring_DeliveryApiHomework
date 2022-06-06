ecddc2ac043eff16b9ad03c9b018ec2dc4210aa7
printf-jin <dlghwls7662@naver.com>
2022-06-06 15:33:56 +0900

    RestaurantResponse 수정

--- src/main/java/com/sparta/deliveryapihomework/dto/RestaurantResponseDto.java
+++ src/main/java/com/sparta/deliveryapihomework/dto/RestaurantResponseDto.java
@@ -1,2 +1,20 @@
-package com.sparta.deliveryapihomework.dto;public class RestaurantResponseDto {
+package com.sparta.deliveryapihomework.dto;
+
+import com.sparta.deliveryapihomework.model.Restaurant;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor
+public class RestaurantResponseDto {
+    private Long id;
+    private String name;
+    private int minOrderPrice;
+    private int deliveryFee;
+
+    public RestaurantResponseDto(Long id, String name, int minOrderPrice, int deliveryFee) {
+        this.id = id;
+        this.name = name;
+        this.minOrderPrice = minOrderPrice;
+        this.deliveryFee = deliveryFee;
+    }
 }
