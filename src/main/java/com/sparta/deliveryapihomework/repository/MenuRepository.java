2099f028cde0ea1ed4d4549b2ebe71deb8d66a71
printf-jin <dlghwls7662@naver.com>
2022-06-06 15:36:14 +0900

    MenuRepository 쿼리문 추가
    레스토랑id로 Menu 정보 가져오기

--- src/main/java/com/sparta/deliveryapihomework/repository/MenuRepository.java
+++ src/main/java/com/sparta/deliveryapihomework/repository/MenuRepository.java
@@ -2,7 +2,13 @@ package com.sparta.deliveryapihomework.repository;
 
 import com.sparta.deliveryapihomework.model.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
 
+    @Query("Select this_ from Menu this_ where this_.restaurant.id = :id")
+    List<Menu> findByRestaurnatId(@Param("id") Long id);
 }
