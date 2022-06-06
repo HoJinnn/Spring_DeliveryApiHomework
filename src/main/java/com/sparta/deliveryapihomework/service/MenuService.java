7a4b00a6ee182a414cbc5959b5a98e92ac630969
printf-jin <dlghwls7662@naver.com>
2022-06-06 15:38:41 +0900

    MenuService 수정

--- src/main/java/com/sparta/deliveryapihomework/service/MenuService.java
+++ src/main/java/com/sparta/deliveryapihomework/service/MenuService.java
@@ -1,7 +1,25 @@
 package com.sparta.deliveryapihomework.service;
 
+import com.sparta.deliveryapihomework.model.Menu;
+import com.sparta.deliveryapihomework.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class MenuService {
+
+    private final MenuRepository repository;
+
+    public void registerMenu(List<Menu> menus) {
+        repository.saveAll(menus);
+    }
+
+    public List<Menu> findByRestaurantId(Long restaurantId) {
+        return repository.findByRestaurnatId(restaurantId);
+    }
+
+
 }
