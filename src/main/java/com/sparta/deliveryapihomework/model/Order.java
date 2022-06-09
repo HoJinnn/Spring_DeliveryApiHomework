5732bfadd0ab62214e680755923b1b925e8bb529
printf-jin <dlghwls7662@naver.com>
2022-06-09 10:27:33 +0900

    Order 엔티티 클래스 양방향 매핑 추가
    TotalPrice 계산 메소드 추가

--- src/main/java/com/sparta/deliveryapihomework/model/Order.java
+++ src/main/java/com/sparta/deliveryapihomework/model/Order.java
@@ -1,10 +1,17 @@
 package com.sparta.deliveryapihomework.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
+@Table(name = "ORDERS")
+@NoArgsConstructor
 @Entity
 public class Order extends Timestamped {
 
@@ -13,10 +20,30 @@ public class Order extends Timestamped {
     @Column(name = "Order_ID")
     private Long id;
 
-    @Column(nullable = false)
     private int totalPrice;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "RESTAURNAT_ID")
     Restaurant restaurant;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "order")
+    private List<OrderMenu> orderMenus = new ArrayList<>();
+
+
+    public void countTotalPrice(List<OrderMenu> orderMenus) {
+        int sum = 0;
+        for (OrderMenu orderMenu : orderMenus) {
+            sum += orderMenu.getPrice();
+        }
+        sum += this.restaurant.getDeliveryFee();
+
+        this.totalPrice = sum;
+    }
+
+    @Builder
+    public Order(Restaurant restaurant, int totalPrice) {
+        this.restaurant = restaurant;
+        this.totalPrice = totalPrice;
+    }
 }
